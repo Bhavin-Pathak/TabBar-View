34cea40cd722e591abf3bc6874e53b361dd46ad7
rvshah2217 <rvshah2217@gmail.com>
2023-09-22 20:40:28 +0530

    Initial Commit

---
+++ TabBar-View/TabBar_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar_ViewApp.swift
+//  TabBar-View
+//
+//  Created by Ravi Shah on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TabBar_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
